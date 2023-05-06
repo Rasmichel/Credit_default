6bc4d77abaff378f35e5b94f212fd61b95b86103
Segui GNABA <angerichard17@gmail.com>
2023-05-06 02:44:46 +0200

    First save

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predict credit default probability ',
+    author='Segui Ange-Michel GNABA',
+    license='MIT',
+)
